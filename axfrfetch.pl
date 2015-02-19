c917665c6580eadb33753990659deb5e2722aaf4
Caoimhe Chaos <caoimhechaos@protonmail.com>
2015-02-19 02:02:18 +0100

    Write the transferred zone to a file.
    Before fetching, check whether it needs updating by comparing the
    serial number. For ease of implementation, the zone is fetched on
    any kind of difference between the versions, not just increments.

--- axfrfetch.pl
+++ axfrfetch.pl
@@ -4,16 +4,51 @@
 use strict;
 use warnings;
 
+use File::Slurp qw(read_file write_file);
 use Net::DNS;
 
 my @supported_types = qw(SOA A AAAA CNAME TXT MX NAPTR NS SRV PTR);
 my %support;
 my $resolver = new Net::DNS::Resolver();
 my @reply;
+my $text;
+my $serial = 0;
+my $remote_serial = 0;
+my $answer;
+my $fh;
 
 my ($domain, $ns) = @ARGV;
 
+die('Usage: ' . __FILE__ . ' <domain> <nameserver>')
+	unless (defined($domain) && defined($ns));
+
 $resolver->nameserver($ns);
+
+eval {
+	$text = read_file($domain);
+};
+unless ($@) {
+	if ($text =~ /$domain.*SOA\D+(\d*)\D/) {
+		$serial = $1;
+	}
+}
+
+if (int($serial) > 0) {
+	$answer = $resolver->query($domain, 'SOA');
+	@reply = $answer->answer;
+
+	foreach $answer (@reply) {
+		if ($answer->type eq 'SOA') {
+			$remote_serial = $answer->serial;
+		}
+	}
+}
+
+if ($serial && $remote_serial && $serial eq $remote_serial) {
+	printf("%s is already up to date at %d\n",
+		$domain, $serial);
+	exit(0);
+}
 
 @reply = $resolver->axfr($domain);
 
@@ -23,18 +58,23 @@ for my $type (@supported_types) {
 	$support{$type} = 1;
 }
 
+$fh = IO::File->new('> ' . $domain);
+unless (defined($fh)) {
+	die('Unable to open ' . $domain);
+}
+
 for my $rr (@reply) {
 	if ($rr->type eq 'SOA') {
-		printf("\$ORIGIN %s.\n", $rr->name);
+		printf($fh "\$ORIGIN %s.\n", $rr->name);
 	}
 
 	if ($support{$rr->type}) {
-		printf("%s\n", $rr->string);
+		printf($fh "%s\n", $rr->string);
 	} else {
-		printf("%s.\t%d\t%s\tTYPE%d\t\\# %d %s\n",
+		printf($fh "%s.\t%d\t%s\tTYPE%d\t\\# %d %s\n",
 			$rr->name, $rr->ttl, $rr->class,
 		       	Net::DNS::typesbyname($rr->type),
 			length($rr->rdata),
-			unpack("H*", $rr->rdata));
+			unpack('H*', $rr->rdata));
 	}
 }
